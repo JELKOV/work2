414dc999116f43644147f0952aa663adb6b40569
AHNJAEHO <AHNJAEHO@LAPTOP-3GN2E06V>
2024-08-08 21:24:21 +0900

    수정

--- useBeansSignup/src/main/java/member/MemberDTO.java
+++ useBeansSignup/src/main/java/member/MemberDTO.java
@@ -1,29 +1,41 @@
 package member;
 
+
 public class MemberDTO {
-	private String mid;
-	private String password;
-	private String name;
-	public String getMid() {
-		return mid;
-	}
-	public void setMid(String mid) {
-		this.mid = mid;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	@Override
-	public String toString() {
-		return "MemberDTO [mid=" + mid + ", password=" + password + ", name=" + name + "]";
-	}
+    private String mid;
+    private String password;
+    private String name;
+    private String condition;
+
+    public String getMid() {
+        return mid;
+    }
+
+    public void setMid(String mid) {
+        this.mid = mid;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
 }
